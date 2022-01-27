1f43348ae8ac051d168b3ebbc1b0558ab6dbf96a
Russell Senior <russell@personaltelco.net>
2022-01-27 15:41:01 -0800

    version 8
    Signed-off-by: Russell Senior <russell@personaltelco.net>

--- jup.c
+++ jup.c
@@ -1,260 +1,243 @@
-L/*************************************************************************/
-/* JUP6.C:  A program to calculate the trajectory of a spacecraft which  */
-/*          is encountering the planet Jupiter, in the presense of the   */
-/*          moon IO.  This version reads input data from a textfile      */
-/*          specified on the command line.                               */
-/*************************************************************************/
+/*
+**     J U P I T E R - I O - P R O B E   S I M U L A T I O N
+**
+**     Written by:  Russell S. Senior
+**     Date:  09 December 1987
+**
+**     This program solve the trajectory of a probe through a 
+**     gravitational environment including a stationary planet
+**     and a moon moving in a circular orbit.  The method used
+**     is the Runge-Kutta.  The step size will be constant.
+**
+**     Version 1.0
+*/
 
 #include <stdio.h>
 #include <math.h>
 
-typedef struct {double x,y,r,t;} VECTOR;
+typedef struct { double x,y,r,t; } VECTOR;
 
-#define RADIUS   4.218070627e8
-#define PERIOD   152859.0
-#define pullio   5.2477160e12
-#define pulljup  1.2675868e17
-#define radJUP   7.137e7
-#define radIO    1.726e6
-#define mass     1.138e3
+#define  PI       3.141592653589793
+#define  PERIOD   152859.0
+#define  RADIUS   4.218070627e8
+#define  PULLJUP  1.2675868e17
+#define  PULLIO   5.2477160e12
+#define  RADJUP   7.137e7
+#define  RADIO    1.726e6
+#define  MASS     1.138e3
+#define  sq(x)    ((x)*(x))
 
-double INTER,dt;
+VECTOR iopos0;    /* position of io at start of interval    */
+VECTOR iopos1;    /* position of io at mid-interval         */
+VECTOR iopos2;    /* position of io at end of interval      */
+VECTOR p0;        /* position of probe at start of interval */
+VECTOR p1;        /* predicted position at mid-interval     */
+VECTOR p2;        /* predicted position at end of interval  */
+VECTOR a;         /* average acceleration over interval     */
+VECTOR a0;        /* acceleration at p0                     */
+VECTOR a1;        /* acceleration at first p1               */
+VECTOR a2;        /* acceleration at second p1              */
+VECTOR a3;        /* acceleration at p2                     */
+VECTOR v;         /* velocity of probe at start of interval */
 
-/************************************************/
-/*            M A I N   P R O G R A M           */
-/************************************************/
+VECTOR io (double timep);
+VECTOR acc (VECTOR *p,VECTOR *i,int useio);
 
-FILE *out,*in;
+double dt;        /* time interval for calculations         */
+double timep;     /* elapsed time in simulation             */
+double fint;      /* time interval between dumps to file    */
+double sint;      /* time interval between dumps to screen  */
+double fdump;     /* time at which file dump occurs         */
+double sdump;     /* time at which screen dump occurs       */
+double phi;       /* offset angle for io                    */
+double maxtime;   /* time at which simulation stops         */
+double e;         /* total energy of probe                  */
+double l;         /* angular momentum of probe              */
+double al;        /* alpha - parameter of the trajectory    */
+double ep;        /* epsilon - parameter of the trajectory  */
+double r;         /* predicted closest approach to jupiter  */
 
+double energy(VECTOR *pos,VECTOR *vel,VECTOR *io,int useio);
+double angular(VECTOR *,VECTOR *);
+
+int    iothere;   /* boolean to indicate presence of io     */
+
+char *probe_time(double);
+
+FILE *in,*out;
+ 
 main (argc,argv)
-   int argc;
-   char *argv[];
+    int argc;
+    char *argv[];
 {
-   VECTOR SCacc,SCvel,SCpos,temp,temp2,IOpos,
-          IO(),ACC();
-   double time = 0.0,l,a,e,R,E,phi,maxtime,
-          energy(),L(),alpha(),epsilon(),Rmin(),
-          sq();
-   int i = 1,j = 1,skip,skip2,logic;
+    if (argc != 3)
+    {
+        fprintf(stderr,"\nUsage: %s infile outfile\n",argv[0]);
+        exit(0);
+    }
+    in = fopen(argv[1],"r");
+    out = fopen(argv[2],"w");
 
-   if (argc >= 2)
-      in = fopen(argv[1],"r");
-   else
-      in = fopen("STDIN","r");
-   if (argc = 3)
-      out = fopen(argv[2],"w");
-   else
-      out = fopen("GALILEO.DAT","a");
-   fscanf (in,"%lf",&INTER);
-   dt = INTER;
-   fscanf (in,"%d", &skip);
-   fscanf (in,"%d", &skip2);
-   fscanf (in,"%lf %lf",&SCpos.x,&SCpos.y);
-   SCpos.r = sqrt(sq(SCpos.x) + sq(SCpos.y));
-   SCpos.t = atan2(SCpos.y,SCpos.x);
-   fscanf (in,"%lf %lf",&SCvel.x,&SCvel.y);
-   SCvel.r = sqrt(sq(SCvel.x) + sq(SCvel.y));
-   SCvel.t = atan2(SCvel.y,SCvel.x);
-   fscanf (in,"%d",&logic);
-   fscanf (in,"%lf", &phi);
-   phi = phi * PI / 180.0;
-   fscanf (in,"%lf",&maxtime);
-   IOpos = IO(time,phi);
-   dump(0,time,SCpos,SCvel);
-   dump(1,time,SCpos,IOpos);
-   E = energy (SCpos,SCvel);
-   l = L (SCpos,SCvel);
-   a = alpha (l);
-   e = epsilon (E,l);
-   R = Rmin (a,e);
-   printf("     %12.5e %12.5e %12.5e %12.5e %12.5e\n",E,l,a,e,R);
-   fprintf(out,"%.8e,%.8e,%.8e,%.8f,%.8e",E,l,a,e,R);
-   while (time <= maxtime)
-   {
-      SCacc = ACC(SCpos,IOpos,logic);
-      temp.x = SCpos.x + (SCvel.x * dt) + (SCacc.x * sq(dt));
-      temp.y = SCpos.y + (SCvel.y * dt) + (SCacc.y * sq(dt));
-      temp.r = sqrt(sq(temp.x) + sq(temp.y));
-      temp.t = atan2(temp.y,temp.x);
-      IOpos = IO(time+dt,phi);
-      temp2 = ACC(temp,IOpos,logic);
-      SCacc.x = (SCacc.x + temp2.x) / 2.0;
-      SCacc.y = (SCacc.y + temp2.y) / 2.0;
-      SCpos.x = SCpos.x + (SCvel.x * dt) + (SCacc.x * sq(dt));
-      SCpos.y = SCpos.y + (SCvel.y * dt) + (SCacc.y * sq(dt));
-      SCpos.r = sqrt(sq(SCpos.x) + sq(SCpos.y));
-      SCpos.t = atan2(SCpos.y,SCpos.x);
-      SCvel.x = SCvel.x + (SCacc.x * dt);
-      SCvel.y = SCvel.y + (SCacc.y * dt);
-      SCvel.r = sqrt(sq(SCvel.x) + sq(SCvel.y));
-      SCvel.t = atan2(SCvel.y,SCvel.x);
-      time += dt;
-      if (i == skip)
-      {
-         dump(0,time,SCpos,SCvel);
-         i = 1;
-         E = energy (SCpos,SCvel);
-         l = L (SCpos,SCvel);
-         a = alpha (l);
-         e = epsilon (E,l);
-         R = Rmin (a,e);
-         printf("     %12.5e %12.5e %12.5e %12.5e %12.5e\n",E,l,a,e,R);
-      }  else i++;
-      if (j == skip2)
-      {
-         dump(1,time,SCpos,IOpos);
-         j = 1;
-         E = energy (SCpos,SCvel);
-         l = L (SCpos,SCvel);
-         a = alpha (l);
-         e = epsilon (E,l);
-         R = Rmin (a,e);
-         fprintf(out,"%.8e,%.8e,%.8e,%.8f,%.8e",E,l,a,e,R);
-      }  else j++;
-   }
-   fclose(out);
+    fscanf(in,"%lf %lf %lf",&dt,&fint,&sint);
+    fscanf(in,"%lf %lf %lf %lf",&p0.x,&p0.y,&v.x,&v.y);
+    fscanf(in,"%d %lf %lf",&iothere,&phi,&maxtime);
+    phi *= PI / 180.0;
+
+    p0.r = sqrt(sq(p0.x) + sq(p0.y));
+    iopos0 = io(0.0);
+
+    fdump = 0.0;
+    sdump = 0.0;
+
+    while (timep < maxtime)
+    {
+        if (timep >= fdump || timep >= sdump)
+        {
+            p0.r = sqrt(sq(p0.x) + sq(p0.y));
+            e = energy(&p0,&v,&iopos0,iothere);
+            l = angular(&p0,&v);
+            al = sq(l) / (sq(MASS) * PULLJUP);
+            ep = sqrt (1 + (2.0 * e * sq(l/(MASS*PULLJUP)) / MASS));
+            r = al / (1 + ep);
+
+            if (timep >= fdump)
+            {
+                fprintf(out,
+                    "\n%.0f,%18.10e,%18.10e,%18.10e,%18.10e,%18.10e,%18.10e,"
+                    "%18.10e,%18.10e,%18.10e,%18.10e,%18.10e",
+                    timep,p0.x,p0.y,v.x,v.y,iopos0.x,iopos0.y,e,l,al,ep,r);
+                fdump += fint;
+            }
+            if (timep >= sdump)
+            {
+                fprintf(stdout,
+                    "%s %12.5e %12.5e %12.5e %12.5e\n"
+                    "     %12.5e %12.5e %12.5e %12.5e %12.5e\n",
+                    probe_time(timep),p0.x,p0.y,v.x,v.y,e,l,al,ep,r);
+                sdump += sint;
+            }
+        }
+        iopos1 = io (timep + dt/2.0);
+        iopos2 = io (timep + dt);
+        a0 = acc (&p0,&iopos0,iothere);
+        p1.x = p0.x + v.x*dt/2.0 + a0.x*sq(dt/2.0)/2.0;
+        p1.y = p0.y + v.y*dt/2.0 + a0.y*sq(dt/2.0)/2.0;
+        a1 = acc (&p1,&iopos1,iothere);
+        p1.x = p0.x + v.x*dt/2.0 + a1.x*sq(dt/2.0)/2.0;
+        p1.y = p0.y + v.y*dt/2.0 + a1.y*sq(dt/2.0)/2.0;
+        a2 = acc (&p1,&iopos1,iothere);
+        p2.x = p0.x + v.x*dt + a2.x*sq(dt)/2.0;
+        p2.y = p0.y + v.y*dt + a2.y*sq(dt)/2.0;
+        a3 = acc (&p2,&iopos2,iothere);
+
+        a.x = (a0.x + 2.0*a1.x + 2.0*a2.x + a3.x) / 6.0;
+        a.y = (a0.y + 2.0*a1.y + 2.0*a2.y + a3.y) / 6.0;
+        p0.x += v.x*dt + a.x*sq(dt)/2.0;
+        p0.y += v.y*dt + a.y*sq(dt)/2.0;
+        v.x += a.x*dt;
+        v.y += a.y*dt;
+        iopos0.x = iopos2.x;
+        iopos0.y = iopos2.y;
+        timep += dt;
+    }
+    fclose (in);
+    fclose (out);
 }
 
-/************************************************/
+/*************************************/
 
-VECTOR ACC (location,locIO,on)
-   VECTOR location,locIO;
-   int on;
+VECTOR acc (p,i,useio)
+    VECTOR *p;
+    VECTOR *i;
+    int useio;
 {
-   VECTOR toJUP,toIO,posIO,total;
-   toJUP.r = - pulljup / sq(location.r);
-   if (location.r < radJUP) printf("\n\nCRASH...CRASHED ON JUPITER!\n\n");
-   toJUP.x = toJUP.r * location.x / location.r;
-   toJUP.y = toJUP.r * location.y / location.r;
-   if (on != 0)
-   {
-      posIO.x = location.x - locIO.x;
-      posIO.y = location.y - locIO.y;
-      posIO.r = sqrt(sq(posIO.x) + sq(posIO.y));
-      if (posIO.r < radIO) printf("\n\nCRASH...CRASHED ON IO!\n\n");
-      toIO.r  = - pullio  / sq(posIO.r);
-      toIO.x  = toIO.r * posIO.x / posIO.r;
-      toIO.y  = toIO.r * posIO.y / posIO.r;
-      total.x = toJUP.x + toIO.x;
-      total.y = toJUP.y + toIO.y;
-      total.r = sqrt(sq(total.x) + sq(total.y));
-      total.t = atan2(total.y,total.x);
-      return total;
-   }
-   else
-   {
-      toJUP.t = atan2(toJUP.y,toJUP.x);
-      return toJUP;
-   }
+    VECTOR total;
+    VECTOR ptoi;
+    
+    p->r = sqrt(sq(p->x) + sq(p->y));
+    total.r = - PULLJUP / sq(p->r);
+    total.x = total.r * (p->x) / (p->r);
+    total.y = total.r * (p->y) / (p->r);
+    if (useio)
+    {
+        ptoi.x = (p->x) - (i->x);
+        ptoi.y = (p->y) - (i->y);
+        ptoi.r = sqrt(sq(ptoi.x) + sq(ptoi.y));
+        total.r = -PULLIO / sq(ptoi.r);
+        total.x += total.r * ptoi.x / ptoi.r;
+        total.y += total.r * ptoi.y / ptoi.r;
+    }
+    return total;
 }
 
-/************************************************/
+/*************************************/
 
-VECTOR IO (loctime,phi)
-   double loctime,phi;
+VECTOR io (timep)
+    double timep;
 {
-   VECTOR pos;
-   pos.r = RADIUS;
-   pos.t = (loctime * 2.0 * PI / PERIOD) + phi;
-   pos.x = pos.r * cos(pos.t);
-   pos.y = pos.r * sin(pos.t);
-   return pos;
+    VECTOR pos;
+
+    pos.r = RADIUS;
+    pos.t = (timep * 2.0 * PI / PERIOD) + phi;
+    pos.x = pos.r * cos(pos.t);
+    pos.y = pos.r * sin(pos.t);
+    return pos;
 }
 
-/************************************************/
+/*************************************/
 
-double sq (k)
-   double k;
+double energy(pos,vel,io,useio)
+    VECTOR *pos;
+    VECTOR *vel; 
+    VECTOR *io;
+    int useio;
 {
-   return (k * k);
+    double distance,total;
+    distance = sqrt(sq((pos->x)-(io->x)) + sq((pos->y)-(io->y)));
+    (vel->r) = sqrt(sq(vel->x) + sq(vel->y));
+    total = (0.5 * sq(vel->r)) - (PULLJUP / (pos->r));
+    if (useio)
+        total -= (PULLIO / distance);
+    return (total * MASS);
 }
 
-/************************************************/
+/*************************************/
 
-TIME (second)
-   double second;
+double angular(pos,vel)
+    VECTOR *pos;
+    VECTOR *vel;
 {
-   short hour=0,min=0,logic;
-   if (second < 0.0)
-   {
-      second = - second;
-      logic = 1;
-      printf("-");
-   }
-   else
-      printf(" ");
-   while (second >= 3600.0)
-   {  second = second - 3600.0; hour++; }
-   while (second >= 60)
-   {  second = second - 60.0; min++;    }
-   if (hour < 10) printf("0");
-   printf("%d:",hour);
-   if (min < 10) printf("0");
-   printf("%d:",min);
-   if (second < 10.0) printf("0");
-   printf("%.2f",second);
-   return ;
-}
+    double a,b;
+    a = (pos->x) / (pos->r);
+    b = (pos->y) / (pos->r);
+    return ((pos->r) * MASS * ((a * (vel->y)) - (b * (vel->x))));
+}   
+ 
+/*************************************/
 
-/************************************************/
-
-dump (k,l,m,n)
-   short k;
-   VECTOR m,n;
-   double l;
+char *probe_time (seconds)
+    double seconds;
 {
-   if (k == 0)
-   {   TIME (l);
-      printf("  %12.5e %12.5e %12.5e %12.5e\n",m.x,m.y,n.x,n.y);
-   }
-   if (k == 1)
-   {
-      fprintf(out,"\n%.0f,%.8e,%.8e,%.8e,%.8e,",l,m.x,m.y,n.x,n.y);
-   }
-   return ;
+    static char buffer[20];
+    int hrs=0,min=0;
+
+    if (seconds < 0.0)
+    {
+        seconds *= -1;
+        buffer[0] = '-';
+    }
+    else
+        buffer[0] = ' ';
+    while (seconds >= 3600.0)
+    {
+        seconds -= 3600.0;
+        hrs++;
+    }
+    while (seconds >= 60.0)
+    {
+        seconds -= 60.0;
+        min++;
+    }
+    sprintf(buffer+1,"%03d:%02d:%05.2f",hrs,min,seconds);
+    return buffer;
 }
-
-/************************************************/
-
-double energy (pos,vel)
-   VECTOR pos,vel;
-{
-   return (0.5 * mass * sq(vel.r)) - (pulljup * mass / pos.r);
-}
-
-/************************************************/
-
-double L (pos,vel)
-   VECTOR pos,vel;
-{
-   double m,n;
-   m = pos.x / pos.r;
-   n = pos.y / pos.r;
-   return (pos.r * mass * ((m * vel.y) - (n * vel.x)));
-}
-
-/************************************************/
-
-double alpha (l)
-   double l;
-{
-   return (sq(l) / (sq(mass) * pulljup));
-}
-
-/************************************************/
-
-double epsilon (E,l)
-   double E,l;
-{
-   return sqrt(1 + (2.0 * E * sq(l / (mass * pulljup)) / mass));
-}
-
-/************************************************/
-
-double Rmin (a,e)
-   double a,e;
-{
-   return (a / (1 + e));
-}
-
-/************************************************/
